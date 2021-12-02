decfa79e28f22956d8dceaf53f5c0fe0671043da
Trevor Kinsie <trevor.kinsie@gmail.com>
2021-12-02 08:31:19 -0500

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,21 +1,70 @@
 inputFileName = "input.txt"
 
-previous = None
-current = None
-increaseCount = 0
 
-with open(inputFileName) as inputList:
+def part_1():
+  previous = 0
+  current = 0
+  increaseCount = 0
 
-  for row in inputList:
-    current = row.strip()
+  with open(inputFileName) as inputList:
 
-    if previous is None:
+    for row in inputList:
+      current = int(row.strip())
+
+      if previous == 0:
+        previous = current
+        continue
+      elif current > previous:
+        increaseCount += 1 
+
       previous = current
-      continue
-    elif previous < current:
-      increaseCount += 1 
 
-    previous = current
+  return increaseCount
+
+def part_2():
+  # chunkSize = 3
+  # arrayList = []
+  # previous = 0
+  # increaseCount = 0
+
+  # with open(inputFileName) as inputList:
+  #   arrayList = inputList.read().split('\n')
+
+  # # for row in [arrayList[i:i+chunkSize] for i in range(len(arrayList))[::chunkSize]]:
+  # #   total = sum(list(map(int, row)))
+  # #   if total > previous:
+  # #     increaseCount += 1
+
+  # #   previous = total
+
+  # # return increaseCount
 
 
-print (increaseCount)
+  # for x in range(len(arrayList)):
+  #   if x+1 == len(arrayList):
+  #     current = arrayList[x]
+  #   elif x+2 == len(arrayList):
+  #     current = arrayList[x] + arrayList[x+1]
+  #   else:
+  #     current = arrayList[x] + arrayList[x+1] + arrayList[x+2]
+
+
+  #   if previous == 0:
+  #     previous = current
+  #     continue
+  #   elif current > previous:
+  #     increaseCount += 1 
+
+  #   previous = current
+  with open(inputFileName, 'r') as f:
+      nums = [int(i) for i in f.read().splitlines()]
+
+  return sum(nums[i] > nums[i-3] for i in range(3, len(nums)))
+
+
+## Group each into 
+
+
+if __name__ == "__main__":
+  print (part_1())
+  print (part_2())
